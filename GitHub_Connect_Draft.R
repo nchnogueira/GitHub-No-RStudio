76e4f813fe2e69a0bc27b6d32c754e28e167642c
NicolasNogueira92 <NicolasNogueira92@ufrj.br>
2022-08-25 15:23:56 -0300

    Digite uma mensagem

---
+++ GitHub_Connect_Draft.R
@@ -0,0 +1,32 @@
+# Instalar o R
+# Instalar o RStudio
+# Instalar o Git
+
+# No terminal execute
+git config --global user.name 'PExCCA-LAMET-UENF'
+git config --global user.email 'pexcca,lamet@uenf.br'
+
+# Instalar pacote usethis
+usethis::use_git_config(user.name = "PExCCA-LAMET-UENF",
+                        user.email = "pexcca.lamet@uenf.br")
+
+# Criar um token de autenticação em sua conta do GitHub
+usethis::create_github_token()
+
+# Cole o token aqui
+gitcreds::gitcreds_set()
+
+# Reinicie o R   CTRL+SHIFT+F10
+
+# Checando se tudo deu certo
+usethis::git_sitrep()
+# > Git config (global)
+#> ● Name: 'SEU NOME DEVE APARECER AQUI'
+#> ● Email: 'SEU EMAIL DEVE APARECER AQUI'
+#> GitHub
+#> ● Default GitHub host: 'https://github.com'
+#> ● Personal access token for 'https://github.com': '<discovered>'
+#> ● GitHub user: 'SEU NOME DE USUÁRIO(A) DEVE APARECER AQUI'
+#> ● Token scopes: 'gist, repo, user, workflow'
+#> ● Email(s): 'SEU EMAIL DEVE APARECER AQUI'
+
